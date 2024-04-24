62ba3c0f5b3d7b47bb49c1a491730afd0a534a5e
laredo02 <miguellaredobarbadillo@gmail.com>
2024-04-24 17:13:45 +0200

    cifar10 conv2d

--- pytorch-nn/cifar10/model.py
+++ pytorch-nn/cifar10/model.py
@@ -1,7 +1,12 @@
 
 import torch
+import torch.nn as nn
+import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
 
 train_transforms = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
@@ -23,10 +28,52 @@ test_data = datasets.CIFAR10(root="~/.pytorch/cifar10", train=False, download=Tr
 print("Training Data\n", train_data)
 print("Test Data\n", test_data)
 
-trainloader = DataLoader(train_data, batch_size=32, shuffle=True)
-testloader = DataLoader(test_data, batch_size=32, shuffle=True)
+train_loader = DataLoader(train_data, batch_size=64, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=64, shuffle=True)
 
-data_batch, labels_batch = next(iter(trainloader))
+class NeuralNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=(3, 3), padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=(3, 3), padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=(3, 3), padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(128*4*4, 512)
+        self.fc2 = nn.Linear(512, 10)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = self.pool(self.relu(self.conv3(x)))
+        x = x.view(-1, 128 * 4 * 4)
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+model = NeuralNet().to(device)
+print(model)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+
+def train_model(epochs):
+    model.train()
+    for i in range(epochs):
+        for batch, (X, y) in enumerate(train_loader, 0):
+            X, y = X.to(device), y.to(device)
+            optimizer.zero_grad()
+            pred = model(X)
+            loss = criterion(pred, y)
+            loss.backward()
+            optimizer.step()
+            if batch%100 == 0:
+                print(loss.item())
+
+def test_model(epochs):
+
+train_model(10)
+
 
 
 
