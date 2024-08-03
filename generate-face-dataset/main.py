8586ad1499c44f148149c1e479175224efc50cf6
laredo02 <miguellaredobarbadillo@gmail.com>
2024-08-03 03:18:37 +0200

    iface

--- generate-face-dataset/main.py
+++ generate-face-dataset/main.py
@@ -1,7 +1,24 @@
 
 import cv2 as cv
 
+cap = cv.VideoCapture(0)
 
+if not cap.isOpened():
+    print("ERROR: Unable to open video capture device.")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+    frame = cv.flip(frame, 1)    
+    
+    if ret:
+        cv.imshow('Webcam Video', frame)
+        if cv.waitKey(1) & 0xff == ord('q'):
+            break
+    else:
+        print("ERROR: Unable to read frame.")
+        break
+    
 
 
 
